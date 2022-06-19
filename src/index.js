8e507e47f98dd33c27805e169471418f568c16fe
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-19 18:36:49 +0300

    render characters to dom with click

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
 // Your code here
 document.addEventListener("DOMContentLoaded", () => {
-  function fetchData() {
-    fetch("http://localhost:3000/characters")
-      .then((resp) => resp.json())
-      .then((data) => rendercharacter(data));
-  }
-  const characterbar = document.querySelector("#character-bar");
-  //funtion to render character to nav
-  function rendercharacter(data) {
-    //console.log(data);
-    let output = "<span>Posts</span>";
-    data.forEach((post) => {
-      output += `
-        <span>${post.name}</span>
-          `;
-    });
-    characterbar.innerHTML = output;
-  }
   fetchData();
 });
+//get dom elements
+const characterBar = document.getElementById("character-bar");
+const characterName = document.getElementById("name");
+const characterImage = document.getElementById("image");
+const characterVote = document.getElementById("vote-count");
+
+function fetchData() {
+  fetch("http://localhost:3000/characters")
+    .then((resp) => resp.json())
+    .then((data) => {
+      renderCharacters(data);
+    });
+}
+
+//funtion to render characters
+function renderCharacters(data) {
+  data.forEach((data) => {
+    const nameSpan = document.createElement("span");
+    nameSpan.innerText = data.name;
+
+    characterBar.appendChild(nameSpan);
+    nameSpan.addEventListener("click", () => {
+      characterName.textContent = data.name;
+      characterImage.setAttribute("src", data.image);
+      characterVote.textContent = data.votes;
+    });
+  });
+}
+
+// function updateVotes(characters) {
+//   // console.log(characters)
+//   for (let element of characters) {
+//     const characterVoteForm = document.getElementById("votes-form");
+
+//     characterVoteForm.addEventListener("submit", (e) => {
+//       e.preventDefault();
+//       // console.log(e.target.votes.value)
+//       const newVotes = e.target.votes.value;
+//       const characterVote = document.getElementById("vote-count");
+//       characterVote.textContent = newVotes;
+//       fetch("http://localhost:3000/characters", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(newVotes),
+//       }).then((res) => console.log(res.json()));
+//       // .then(votes => console.log(votes))
+//     });
+//   }
+// }
