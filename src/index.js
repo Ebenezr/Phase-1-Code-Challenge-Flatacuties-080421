69ac272286b1facb83e8ce10847ceda706269049
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-19 17:21:57 +0300

    render character to nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+  function fetchData() {
+    fetch("http://localhost:3000/characters")
+      .then((resp) => resp.json())
+      .then((data) => rendercharacter(data));
+  }
+  const characterbar = document.querySelector("#character-bar");
+  //funtion to render character to nav
+  function rendercharacter(data) {
+    //console.log(data);
+    let output = "<span>Posts</span>";
+    data.forEach((post) => {
+      output += `
+        <span>${post.name}</span>
+          `;
+    });
+    characterbar.innerHTML = output;
+  }
+  fetchData();
+});
